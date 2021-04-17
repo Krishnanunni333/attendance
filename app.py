53076d60fdda32cacf731e11a31efb6670408228
Krishnanunni B <46400630+Krishnanunni333@users.noreply.github.com>
2021-04-17 21:59:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 import streamlit as st
+import sqlite3
+import pandas as pd
+from what import call
+import streamlit.components.v1 as components
+    
+    
+connection = sqlite3.connect('temp.db')
 
+dep = st.selectbox('',('EC', 'IT', 'EEE', 'ME', 'CE',))
 
-department = st.selectbox('',('EC', 'IT', 'EEE', 'ME', 'CE',))
+sem = st.selectbox('',('1', '2', '3', '4', '5','6', '7','8'))
 
-semester = st.selectbox('',('1', '2', '3', '4', '5','6', '7','8'))
+sub = st.selectbox('',('comp', 'phy', 'chem', 'math', 'english',))
 
 
 
 if st.button('Take attendance'):
-    import call
-    call.calls()
     
+    with open("./classes/{}.csv".format(dep+sem+sub),"w") as f:
+        f.write('F_name,L_name,chat_id')
+        f.close()
+    
+    call(dep,sem,sub)
+    
+       
+    
+
+    
+
+
+
     
 
--- app1.py
+++ app1.py
@@ -4,9 +4,10 @@ result = 0
 cord = []
 app = Flask(__name__)
 app.secret_key = "super secret key"
-@app.route('/')
+'''@app.route('/')
 def index1():
     return render_template('index.html')
+    
 @app.route('/handle_data', methods=['GET','POST'])
 def handle_data():
     if request.method == 'POST':
@@ -16,11 +17,38 @@ def handle_data():
         global cord
         result = request.form
         cord = [1,1,1,1]
-    return render_template('handle_data.html')
-@app.route('/student/<id>', methods=['GET','POST'])
-def student(id):
+    return render_template('handle_data.html')'''
+    
+@app.route('/student', methods=['GET','POST'])
+def student():
+    dep = request.args.get('dep')
+    if dep == None:
+        return render_template('error.html')
+    print(dep)
+    sem = request.args.get('sem')
+    if sem == None:
+        return render_template('error.html')
+    print(sem)
+    sub = request.args.get('sub')
+    if sub == None:
+        return render_template('error.html')
+    print(sub)
+    id = request.args.get('id')
+    if id == None:
+        return render_template('error.html')
     print(id)
-    return render_template('student.html',dep=session['dep'],cord = cord)
+    f_name = request.args.get('f_name')
+    if f_name == None:
+        return render_template('error.html')
+    print(f_name)
+    l_name = request.args.get('l_name')
+    if l_name == None:
+        return render_template('error.html')
+    print(l_name)
+    with open("./csv/{}.csv".format(dep+sem+sub),"a") as f:
+        f.write(f_name+","+l_name+","+id+"\n")
+        f.close()
+    return render_template('student.html')
 
 
 if __name__ == '__main__':
